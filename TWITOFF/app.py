d69518f452f16c3f3e126ca867788588202ea8bb
PhatDeluxe <ethan@skamarock.net>
2019-11-12 13:32:17 -0700

    Added tons of features to interact with both the twitter and basilica APIs and get actual users tweets

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 #now we make an app factory
 
@@ -7,12 +8,20 @@ def create_app():
     app = Flask(__name__)
 
     #add our config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #Now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset', users=[])
     return app
